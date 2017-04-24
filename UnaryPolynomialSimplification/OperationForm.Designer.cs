37e1a9efe1f0857ca812c1743161c99951cb6ae5
soplist <sopcase@yahoo.com>
2017-04-24 16:13:18 +0800

    20170424

---
+++ UnaryPolynomialSimplification/OperationForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UnaryPolynomialSimplification
+{
+    partial class OperationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.initialFormulaTextBox = new System.Windows.Forms.TextBox();
+            this.simplifyButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // initialFormulaTextBox
+            // 
+            this.initialFormulaTextBox.Location = new System.Drawing.Point(23, 30);
+            this.initialFormulaTextBox.Name = "initialFormulaTextBox";
+            this.initialFormulaTextBox.Size = new System.Drawing.Size(327, 21);
+            this.initialFormulaTextBox.TabIndex = 0;
+            // 
+            // simplifyButton
+            // 
+            this.simplifyButton.Location = new System.Drawing.Point(366, 30);
+            this.simplifyButton.Name = "simplifyButton";
+            this.simplifyButton.Size = new System.Drawing.Size(75, 23);
+            this.simplifyButton.TabIndex = 1;
+            this.simplifyButton.Text = "simplify";
+            this.simplifyButton.UseVisualStyleBackColor = true;
+            this.simplifyButton.Click += new System.EventHandler(this.simplifyButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(21, 72);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 12);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 114);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.simplifyButton);
+            this.Controls.Add(this.initialFormulaTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox initialFormulaTextBox;
+        private System.Windows.Forms.Button simplifyButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ UnaryPolynomialSimplification/OperationForm.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnaryPolynomialSimplification
+{
+    public partial class OperationForm : Form
+    {
+        public OperationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void simplifyButton_Click(object sender, EventArgs e)
+        {
+            string initialFormula = initialFormulaTextBox.Text;
+
+            //string monoidalRegularString = @"[a-zA-Z]{2}";
+            //string[] monoidals = initialFormula.Split(new char[2] { '+', '-' });
+            //string monoidalsString = "";
+            //for (int i = 0; i < monoidals.Length; i++)
+            //{
+            //    monoidalsString += monoidals[i]+",";
+            //}
+
+            int[] symbolIndex = findCharsIndex(new char[2] { '+', '-' }, initialFormula);
+            string[] monoidals = useSymbolPositionSplit(symbolIndex, initialFormula);
+            List<NameList<string>> classifyMonoidalsList = classifyMonoidalsByPow(monoidals);
+            string monoidalsString = "";
+            //for (int i = 0; i < symbolIndex.Length; i++)
+            //{
+            //    monoidalsString += symbolIndex[i] + ",";
+            //}
+            //for (int i = 0; i < monoidals.Length; i++)
+            //{
+            //    monoidalsString += monoidals[i]+",";
+            //}
+            for (int i = 0; i < classifyMonoidalsList.Count; i++)
+            {
+                //monoidalsString += monoidals[i]+",";
+                for (int j = 0; j < classifyMonoidalsList[i].Count; j++)
+                {
+                    monoidalsString += classifyMonoidalsList[i][j] + ",";
+                }
+            }
+
+            DialogResult dr1 = MessageBox.Show(monoidalsString, "result");
+        }
+
+        private List<NameList<string>> classifyMonoidalsByPow(string[] monoidals)
+        {
+            //NameList<string> 
+            Regex regex = new Regex(@"[\^]\d");
+            List<NameList<string>> list = new List<NameList<string>>();
+
+            //string str5 = "sdfg";
+            //bool result1 = Regex.IsMatch(str1, @"(^[+-]?\d*[.]?\d*$)|\s*");
+
+            for (int i = 0; i < monoidals.Length; i++)
+            {
+                string currentMonoidal = monoidals[i];
+
+                bool isPowMonoidal = Regex.IsMatch(currentMonoidal, @"[\^]\d");
+                if (isPowMonoidal)
+                {
+                    MatchCollection mc = regex.Matches(currentMonoidal);
+                    string name = mc[0].Value;
+
+                    bool nameExist = false;
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        NameList<string> currentList = list[j];
+                        if (currentList.name.Equals(name))
+                        {
+                            nameExist = true;
+                            currentList.Add(currentMonoidal);
+                            break;
+                        }
+                    }
+                    if (!nameExist)
+                    {
+                        NameList<string> nameList = new NameList<string>();
+                        nameList.name = name;
+                        nameList.Add(currentMonoidal);
+                        list.Add(nameList);
+                    }
+                }
+                else
+                {
+                    bool constantMonoidalExist = false;
+                    for (int j = 0; j < list.Count; j++)
+                    {
+                        NameList<string> currentList = list[j];
+                        if (currentList.name.Equals(""))
+                        {
+                            constantMonoidalExist = true;
+                            currentList.Add(currentMonoidal);
+                            break;
+                        }
+                    }
+                    if (!constantMonoidalExist)
+                    {
+                        NameList<string> nameList = new NameList<string>();
+                        nameList.name = "";
+                        nameList.Add(currentMonoidal);
+                        list.Add(nameList);
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        private string[] useSymbolPositionSplit(int[] symbolIndex, string initialFormula)
+        {
+            string[] monoidals = new string[symbolIndex.Length+1];
+            int startPosition = 0;
+            for (int i = 0; i < monoidals.Length; i++)
+            {
+                if(i == 0){
+                    startPosition = 0;
+                }
+                else
+                {
+                    startPosition = symbolIndex[i-1];
+                }
+
+                if (i < monoidals.Length-1)
+                {
+                    monoidals[i] = initialFormula.Substring(startPosition, symbolIndex[i] - startPosition);
+                }
+                else//i == monoidals.Length-1
+                {
+                    monoidals[i] = initialFormula.Substring(startPosition, initialFormula.Length - startPosition);
+                }
+
+                
+            }
+
+            return monoidals;
+        }
+
+        private int[] findCharsIndex(char[] chars, string target)
+        {
+            List<List<int>> allIndexs = new List<List<int>>();
+            char[] targetCharArray = target.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                List<int> indexs = new List<int>();
+                char c = chars[i];
+
+                for (int j = 0; j < targetCharArray.Length; j++)
+                {
+                    if (c == targetCharArray[j])
+                    {
+                        indexs.Add(j);
+                    }
+                }
+
+                allIndexs.Add(indexs);
+            }
+
+            return convertTwoDimensionIntListToOrderOneDimension(allIndexs);
+        }
+
+        private int[] convertTwoDimensionIntListToOrderOneDimension(List<List<int>> indexs){
+            int arrayLengh = 0;
+            for (int i = 0; i < indexs.Count; i++)
+            {
+                arrayLengh += indexs[i].Count;
+            }
+
+            int[] oneDimensionIndexs = new int[arrayLengh];
+
+            int cursor = 0;
+            for (int i = 0; i < indexs.Count; i++)
+            {
+                for (int j = 0; j < indexs[i].Count; j++)
+                {
+                    oneDimensionIndexs[cursor] = indexs[i][j];
+                    cursor++;
+                }
+            }
+
+            Array.Sort(oneDimensionIndexs);
+            return oneDimensionIndexs;
+        }
+    }
+}
